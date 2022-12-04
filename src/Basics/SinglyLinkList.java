4bcae5055109fd5e0fed5fea62f76260d2504fc5
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-04 23:22:41 +0500

    Singly Linked List

--- src/Basics/SinglyLinkList.java
+++ src/Basics/SinglyLinkList.java
@@ -10,6 +10,40 @@ class SinglyLinkList {
         this.size = 0;
     }
 
+    //Head Insertion
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) tail = head;
+
+        size += 1;
+    }
+
+    //Tail Insertion
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+
+    //Display the LL
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("END...");
+    }
+
     private class Node {
         private int value;
         private Node next;
