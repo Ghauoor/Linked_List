9cdcc0571addae173b9a8862d750d88ec4e3aa58
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-04 17:32:06 +0500

    Create A Simple LL

--- src/Basics/Main.java
+++ src/Basics/Main.java
@@ -2,7 +2,25 @@ package Basics;
 
 public class Main {
     public static void main(String[] args) {
+        //Create Simple Linked List
+        Node<Integer> num = new Node<>(10);
+        Node<Integer> num2 = new Node<>(10);
+        Node<Integer> num3 = new Node<>(10);
+        Node<Integer> num4 = new Node<>(10);
+        //Connect the Node With Each Other To create a Linked List
+         Node head = num;
+         head.next = num2;
+         num2.next = num3;
+         num3.next = num4;
+         //Tail
+         num4.next = null;
+
+
+
+
+
         SinglyLinkList list = new SinglyLinkList();
+
 
     }
 }
