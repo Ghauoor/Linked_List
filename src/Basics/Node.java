504b97b86c990d70060540a334443192d89bdb03
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-04 19:37:30 +0500

    Traverse LinkedList

--- src/Basics/Node.java
+++ src/Basics/Node.java
@@ -7,4 +7,15 @@ public class Node<T> {
     Node(T data) {
         this.data = data;
     }
+
+    public void traverse(Node<Integer> head) {
+        Node<Integer> curser = head;
+
+        while (curser != null) {
+            System.out.print(curser.data + "-->");
+            curser = curser.next;
+        }
+        System.out.println("END");
+
+    }
 }
