5fa50be786ce8438fcbf6d32514e14cb6242f409
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-05 23:42:22 +0500

    Impliment the insertFirst and insertLast and InsertAtIndex Methods

--- src/Basics/LL.java
+++ src/Basics/LL.java
@@ -1,28 +1,31 @@
 package Basics;
 
-class SinglyLinkList {
+class LL {
 
     private Node head;
     private Node tail;
     private int size;
 
-    public SinglyLinkList() {
+    public LL() {
         this.size = 0;
     }
 
     //Head Insertion
     public void insertFirst(int val) {
         Node node = new Node(val);
+
         node.next = head;
         head = node;
 
-        if (tail == null) tail = head;
-
+        if (tail == null) {
+            tail = head;
+        }
         size += 1;
     }
 
     //Tail Insertion
     public void insertLast(int val) {
+
         if (tail == null) {
             insertFirst(val);
             return;
@@ -34,7 +37,30 @@ class SinglyLinkList {
     }
 
 
-    //Display the LL
+
+    public void insertIndexAt(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+
+    //Display the LinkedList
     public void display() {
         Node temp = head;
         while (temp != null) {
