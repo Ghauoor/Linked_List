e8eb63fde847b692d749f8a14b19786264baf56f
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-13 11:20:13 +0500

    Reverse Of Doubly Linked List

--- src/Basics/DLL.java
+++ src/Basics/DLL.java
@@ -2,6 +2,7 @@ package Basics;
 
 public class DLL {
     Node head;
+
     public void insertFirst(int val) {
         Node node = new Node(val);
         node.next = head;
@@ -14,11 +15,22 @@ public class DLL {
 
     public void display() {
         Node node = head;
+        //for last index
+        Node last = null;
         while (node != null) {
+            last = node;
             System.out.print(node.val + " -> ");
             node = node.next;
         }
         System.out.println("END");
+
+        //Reverse the DLL
+        while (last != null) {
+            System.out.print(last.val + " --> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+
     }
 
     private class Node {
