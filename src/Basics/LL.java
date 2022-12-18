61f9eacf0e4058cda3e06186a9ec6f4af42ff416
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-19 02:14:55 +0500

    Insertion with Recc

--- src/Basics/LL.java
+++ src/Basics/LL.java
@@ -130,17 +130,16 @@ class LL {
     //insertion using recursion
     public void insertRec(int val, int index) {
         head = insertRec(val, index, head);
-
     }
+
     private Node insertRec(int val, int index, Node node) {
         if (index == 0) {
             Node temp = new Node(val, node);
             size++;
-
             return temp;
         }
 
-        node.next = insertRec(val, index--, node.next);
+        node.next = insertRec(val, index - 1, node.next);
         return node;
     }
 
