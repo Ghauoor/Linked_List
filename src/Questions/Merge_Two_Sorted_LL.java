54a4d51320cf011e9784d36d20c36308e1cf9fef
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-20 03:26:37 +0500

    Merge Two Linked List

--- src/Questions/Merge_Two_Sorted_LL.java
+++ src/Questions/Merge_Two_Sorted_LL.java
@@ -10,12 +10,11 @@ public class Merge_Two_Sorted_LL {
             if (list1.val < list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
-                tail = tail.next;
             } else {
                 tail.next = list2;
                 list2 = list2.next;
-                tail = tail.next;
             }
+            tail = tail.next;
         }
 
         tail.next = (list1 != null) ? list1 : list2;
