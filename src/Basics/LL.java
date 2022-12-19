42cefce7d5f9167e5ec7615743dfc953816cfd83
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-19 20:09:38 +0500

    Merge Two Linked List

--- src/Basics/LL.java
+++ src/Basics/LL.java
@@ -10,6 +10,57 @@ class LL {
         this.size = 0;
     }
 
+    //Merge Two LL
+    public static LL merge(LL first, LL second) {
+        Node f = first.head;
+        Node s = second.head;
+
+        LL ans = new LL();
+
+        while (f != null && s != null) {
+            if (f.value < s.value) {
+                ans.insertLast(f.value);
+                f = f.next;
+            } else {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+        }
+
+        while (f != null) {
+            ans.insertLast(f.value);
+            f = f.next;
+
+        }
+
+        while (s != null) {
+            ans.insertLast(s.value);
+            s = s.next;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        LL first = new LL();
+        LL second = new LL();
+
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        LL ans = LL.merge(first, second);
+
+        ans.display();
+
+    }
+
     //Head Insertion
     public void insertFirst(int val) {
         Node node = new Node(val);
@@ -116,7 +167,6 @@ class LL {
         return false;
     }
 
-
     //Get the DoublyNode ref
     private Node getNode(int index) {
         Node node = head;
@@ -132,6 +182,8 @@ class LL {
         head = insertRec(val, index, head);
     }
 
+    //Questions
+
     private Node insertRec(int val, int index, Node node) {
         if (index == 0) {
             Node temp = new Node(val, node);
@@ -143,7 +195,6 @@ class LL {
         return node;
     }
 
-
     //Display the LinkedList
     public void display() {
         Node temp = head;
@@ -154,8 +205,7 @@ class LL {
         System.out.println("END...");
     }
 
-    //Questions
-
+    //Remove Duplicates
     public void removeDuplicates() {
         Node node = head;
 
@@ -171,26 +221,7 @@ class LL {
         tail.next = null;
 
 
-
     }
-
-    public static void main(String[] args) {
-
-        LL l = new LL();
-        l.insertFirst(2);
-        l.insertFirst(2);
-        l.insertFirst(1);
-        l.insertFirst(1);
-        l.insertFirst(2);
-        l.insertFirst(3);
-        l.insertFirst(4);
-
-        l.removeDuplicates();
-
-        l.display();
-
-    }
-
 
     private class Node {
         private int value;
