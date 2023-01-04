4b9a4782e79730cb9f9e8c8a5016b71fbb0233c1
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-04 12:01:20 +0500

    Remove the given val from Linked List

--- src/Questions/Remove_Val_LL.java
+++ src/Questions/Remove_Val_LL.java
@@ -10,4 +10,22 @@ public class Remove_Val_LL {
 
         return head.val == val ? head.next : head;
     }
+
+    private ListNode remove(ListNode head, int val) {
+        //check for head
+
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        ListNode temp = head;
+        while (head != null) {
+            if (head.next != null && head.next.val == val) {
+                head.next = head.next.next;
+                continue;
+            }
+
+            head = head.next;
+        }
+        return temp;
+    }
 }
