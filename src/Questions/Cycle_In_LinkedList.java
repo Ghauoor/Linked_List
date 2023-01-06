13272a57c1463d02ab175fb7ce43deb70a2f5951
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-06 17:53:20 +0500

    Linked list has a cycle in it or Not.

---
+++ src/Questions/Cycle_In_LinkedList.java
@@ -0,0 +1,16 @@
+package Questions;
+
+public class Cycle_In_LinkedList {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
