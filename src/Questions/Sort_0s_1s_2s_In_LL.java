f090912d5472e6f2d30e62f93eb399af7cfc5085
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-13 23:22:01 +0500

    Given a linked list of 0s, 1s and 2s, sort it.

--- src/Questions/Sort_0s_1s_2s_In_LL.java
+++ src/Questions/Sort_0s_1s_2s_In_LL.java
@@ -35,9 +35,58 @@ public class Sort_0s_1s_2s_In_LL {
 
         return head;
     }
+
+
+    //Another Approach in which Data Replacement is Not Allow 
+    public ListNode segregateList(ListNode head) {
+        ListNode zeroDummy = new ListNode(-1);
+        ListNode oneDummy = new ListNode(-1);
+        ListNode twoDummy = new ListNode(-1);
+
+        ListNode zeroTail = zeroDummy;
+        ListNode oneTail = oneDummy;
+        ListNode twoTail = twoDummy;
+
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == 0) {
+                zeroTail.next = curr;
+                zeroTail = curr;
+            } else if (curr.val == 1) {
+                oneTail.next = curr;
+                oneTail = curr;
+            } else {
+                twoTail.next = curr;
+                twoTail = curr;
+            }
+            curr = curr.next;
+        }
+
+        zeroTail.next = (oneDummy.next != null) ? oneDummy.next : twoDummy.next;
+        oneTail.next = twoDummy.next;
+        twoTail.next = null;
+
+        return zeroDummy.next;
+    }
+
+
     /**
-     * TC: O(N)
-     * SC: O(1)
-     */
+     * In this implementation,
+     * we use 3 dummy nodes (zeroDummy, oneDummy, twoDummy) to represent the three sublists of 0s, 1s, and 2s.
+     * We also use 3 pointers (zeroTail, oneTail, twoTail) to keep track of the tail of each sublist.
+     *
+     * In the while loop,
+     * we iterate through the original linked list and move each node to the appropriate sublist based on its value.
+     *
+     * After the while loop,
+     * we merge the sublists in the correct order and return the head of the merged list (which is the next node of zeroDummy).
+     *
+     * Note that this solution is given in the context of the class you provided,
+     * so it is possible that it will not run as is in your environment.
+     *
+     *  TC: O(N)
+     *  SC: O(1)
+     *
+     * */
 
 }
