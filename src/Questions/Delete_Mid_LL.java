3919ff6189dee4a2e0c81cdc79a58fb35ecc632e
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-18 20:26:27 +0500

    Delete the Middle Node of a Linked List Using three Pointer Approach

--- src/Questions/Delete_Mid_LL.java
+++ src/Questions/Delete_Mid_LL.java
@@ -12,4 +12,24 @@ public class Delete_Mid_LL {
         slow.next = slow.next.next;
         return head;
     }
+
+    private ListNode deleteMid(ListNode head) {
+        if (null == head || null == head.next) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+
+    }
 }
