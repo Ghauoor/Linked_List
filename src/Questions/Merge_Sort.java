779d0404b54d3775e375ab424b0a0db4257ac067
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-21 14:00:56 +0500

    Merge Sort On Linked List

--- src/Questions/Merge_Sort.java
+++ src/Questions/Merge_Sort.java
@@ -106,5 +106,58 @@ public class Merge_Sort {
         return mergeLL(left, right);
 
     }
+
+    /**
+     * This code is Valid on Both Platforms
+     */
+
+    static ListNode findmid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode merge(ListNode left, ListNode right) {
+
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+
+                left = left.next;
+                tail = tail.next;
+
+            } else {
+                tail.next = right;
+
+                right = right.next;
+                tail = tail.next;
+            }
+        }
+
+        tail.next = (left != null) ? left : right;
+        return dummyHead.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = findmid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+        ListNode result = merge(left, right);
+        return result;
+    }
 }
 
