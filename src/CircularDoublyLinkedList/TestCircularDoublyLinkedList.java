fb3d86bd59b8b457b091bfdec373f9050ae55d62
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-02-02 17:47:51 +0500

    CircularDoublyLinkedList

---
+++ src/CircularDoublyLinkedList/TestCircularDoublyLinkedList.java
@@ -0,0 +1,15 @@
+package CircularDoublyLinkedList;
+
+public class TestCircularDoublyLinkedList {
+    public static void main(String[] args) {
+        CircularDoublyLinkedList cdll = new CircularDoublyLinkedList();
+        cdll.createCDLL(1);
+        cdll.insertNode(2, 0);
+        cdll.insertNode(3, 2);
+        cdll.insertNode(4, 5);
+        cdll.traverseCDLL();
+        cdll.deleteCDLL();
+        cdll.traverseCDLL();
+
+    }
+}
